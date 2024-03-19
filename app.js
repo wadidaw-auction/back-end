9851d1d6e9120c2f5cd75cb3cf0d880c4f603bca
Farrrel Devara <farel199@yahoo.com>
2024-03-19 19:57:24 +0700

    feat: start websocket

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 const express = require('express')
 const app = express()
-const port = 3000
-var cors = require('cors')
+const port = 3000 
+const {Server} = require("socket.io")
+const {createServer} = require("http")
+const cors = require('cors')
  
 app.use(cors())
 app.use(express.urlencoded({extended : true}))
 app.use(express.json())
 
+const httpServer = createServer(app)
+const io = new Server(httpServer,{
+  cors : {
+    origin : "http://localhost:5173"
+  }
+})
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
+httpServer.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
