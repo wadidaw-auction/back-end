46a50383d5b7b0cf109be10d07d902a87a2eb71e
Farrrel Devara <farel199@yahoo.com>
2024-03-20 13:22:17 +0700

    feat:changes

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const port = 3000
 const {Server} = require("socket.io")
 const {createServer} = require("http")
 const cors = require('cors')
+const Controller = require('./controllers/controller')
  
 app.use(cors())
 app.use(express.urlencoded({extended : true}))
@@ -15,6 +16,27 @@ const io = new Server(httpServer,{
     origin : "http://localhost:5173"
   }
 })
+
+io.on('connection', (socket) => {
+  // console.log(socket);
+  // console.log(socket.id, "< user");
+  // console.log(socket.handshake.auth, "<handshake");
+
+  socket.emit("message", "Welcome " + socket.id)
+
+  socket.on("placeBid", (param)=>{
+    // console.log(param); // {name,price}
+     
+    Controller.placeBid
+  })
+});
+
+/// user dapat place bid ketika input price > price database
+io.on('placeBid', (socket) => {
+
+})
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
--- controllers/controller.js
+++ controllers/controller.js
@@ -1,5 +1,20 @@
+const { Product } = require('../models')
 class Controller{
 
+    static async placeBid(req,res,next){
+        console.log(param);
+        const {name , price} = param
+
+        try {
+            // const data = await Product.findByPk()
+            await data.update({
+                last_bidder : req.user.id,
+                price
+            })
+        } catch (error) {
+            next(error)
+        }
+    }
 }
 
 module.exports = Controller
