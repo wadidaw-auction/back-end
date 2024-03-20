3c3677f94e09fa8285512f533447a1bc405b694e
ramadianrp <ramadianrp@gmail.com>
2024-03-20 11:45:34 +0700

    revisi model & migration dan nambah controller

--- app.js
+++ app.js
@@ -4,18 +4,23 @@ const port = 3000
 var cors = require('cors')
 const errHandler = require('./middlewares/errHandler')
 const authCont  = require("./controllers/authcont")
- 
+const Controller = require('./controllers/controller')
+const { authen } = require('./middlewares/authen')
+
 app.use(cors())
 app.use(express.urlencoded({extended : true}))
 app.use(express.json())
-app.use(errHandler)
 
-router.post("/login", authCont.login );
-router.post("/register", authCont.register);
+app.post("/login", authCont.login );
+app.post("/register", authCont.register);
+
+app.get("/products", Controller.showAllProduct);
+app.post("/products/add",  authen, Controller.addProduct)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.use(errHandler)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- controllers/controller.js
+++ controllers/controller.js
@@ -1,5 +1,42 @@
-class Controller{
+const { Product, User } = require('../models')
 
+class Controller {
+    static async showAllProduct(req, res, next) {
+        try {
+            const product = await Product.findAll({
+                include:{
+                    model: User,
+                    attributes: ["name"]
+                }
+            });
+
+            res.status(200).json(product);
+        } catch (error) {
+            console.log(error);
+            next(error);
+        }
+    }
+
+    static async addProduct(req, res, next) {
+        try {
+            const {
+                name,
+                price,
+                last_bidder
+            } = req.body;
+            
+            let products = await Product.create({
+                name,
+                price,
+                last_bidder
+            });
+
+            res.status(201).json({ message: "New product has been created", products });
+        } catch (error) {
+            console.log(error.message);
+            next(error);
+        }
+    }
 }
 
 module.exports = Controller
